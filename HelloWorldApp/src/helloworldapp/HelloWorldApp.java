4dec1a26706181508a7316d77db4d3a4ffb29130
Steve Yobanaki <15111976453@163.com>
2017-11-07 05:27:13 +0800

    Welcome message.

--- HelloWorldApp/src/helloworldapp/HelloWorldApp.java
+++ HelloWorldApp/src/helloworldapp/HelloWorldApp.java
@@ -15,7 +15,7 @@ public class HelloWorldApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Welcome to NetBeans!");
     }
     
 }
